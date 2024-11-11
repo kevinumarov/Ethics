795c84a2bfa5a7a789e3397f2920b2271279c91a
kevinumarov <keulifeinfluencer@gmail.com>
2024-11-11 19:48:17 +0900

    Initial Commit

---
+++ Ethics/EthicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthicsApp.swift
+//  Ethics
+//
+//  Created by Kevin Umarov on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EthicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
